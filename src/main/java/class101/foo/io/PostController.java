8ffe87377ff98b4aeb14585bb33f1ca3743e5fb1
임준영 <a79007714@gmail.com>
2022-01-08 20:25:51 +0900

    feat: db에서 es로 변경

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -24,7 +24,6 @@ public class PostController {
     private final PostRepository postRepository;
     private final Producer producer;
     private final ObjectMapper objectMapper;
-    private final PostCacheService postCacheService;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
@@ -34,27 +33,9 @@ public class PostController {
         return post;
     }
 
-    // 2. 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") final Integer page) {
-        if (page.equals(1)) {
-            return postCacheService.getFirstPostPage();
-        } else {
-            return postRepository.findAll(
-                    PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-            );
-        }
-    }
-
-    // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") final Long id) {
-        return postRepository.findById(id).get();
-    }
-
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
-    @GetMapping("/post-search")
+    @GetMapping("/search")
     public List<Post> findPostsByContent(@RequestParam final String content) {
-        return postRepository.findByContentContains(content);
+        return postRepository.findByContent(content);
     }
 }
